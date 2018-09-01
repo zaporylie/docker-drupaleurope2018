359d3ade24440fa0b41acb4005c0f1d804c95f85
Jakub Piasecki <jakub@piaseccy.pl>
2018-09-01 19:36:46 +0200

    Readme

--- drupal-europe/js/slides.js
+++
@@ -1,7 +0,0 @@
-for (var slide of document.getElementsByTagName('section')){
-  if (!(slide.getAttribute('data-background') ||
-      slide.getAttribute('data-background-video') ||
-      slide.getAttribute('data-background-iframe'))){
-    // slide.setAttribute('data-background', 'drupal-europe/img/slide_bg.svg')
-  }
-}
